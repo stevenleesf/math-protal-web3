db433a2b4a2201a46a711cdd036e9e9fc12ce5e1
Lee Soon Fatt <lee.soonfatt98@gmail.com>
2022-02-21 23:39:04 +0800

    Init gh page

---
+++ build/precache-manifest.99c55751f64886634944ab7fa9e24763.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397c66845389100af1147f686a05a79a",
+    "url": "/math-protal-web3/index.html"
+  },
+  {
+    "revision": "a7c0998f7f72b973c82c",
+    "url": "/math-protal-web3/static/css/main.815e6772.chunk.css"
+  },
+  {
+    "revision": "20448463d6bc6348276e",
+    "url": "/math-protal-web3/static/js/2.49c1c2ec.chunk.js"
+  },
+  {
+    "revision": "a7c0998f7f72b973c82c",
+    "url": "/math-protal-web3/static/js/main.1b905d11.chunk.js"
+  },
+  {
+    "revision": "d871928a568e63d2f6c1",
+    "url": "/math-protal-web3/static/js/runtime-main.48749e06.js"
+  }
+]);
